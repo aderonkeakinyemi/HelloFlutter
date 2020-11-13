c25603db706f6a14db62af465fb2e2a6294d05c4
= <=>
2020-11-13 23:58:23 +0100

    Icons added

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,17 @@ void main() {
     //The kind of app I am building
     MaterialApp(
       //The landing page of the app
-      home: Center(
-        child: Text('I am'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
