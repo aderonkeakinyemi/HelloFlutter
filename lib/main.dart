3e069dc2e8404f7ad948caf7e1f2eaa49e0f096f
= <=>
2020-11-13 16:44:24 +0100

    My first flutter APP

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// This is the starting point for my app
+void main() {
+  runApp(
+    //The kind of app I am building
+    MaterialApp(
+      //The landing page of the app
+      home: Center(
+        child: Text('I am'),
+      ),
+    ),
+  );
+}
